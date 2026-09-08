8b4bebddfecaba2820708bb43b8eea625a13dd98
RAVI RANJAN KUMAR <raviranjanmth2022@gmail.com>
2026-09-08 10:25:57 +0530

    ✅ SCALENE - Rating 430 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ codechef/easy/SCALENE---Rating-430/solution.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+while t > 0:
+    a, b, c = map(int, input().split())
+
+    # A scalene triangle has all three sides different
+    if a != b and b != c and a != c:
+        print("YES")
+    else:
+        print("NO")
+
+    t -= 1
