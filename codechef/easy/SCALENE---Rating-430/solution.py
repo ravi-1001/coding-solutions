5e6636a42dc5f6b7b01b1d5b0ae2c2a388d1a239
RAVI RANJAN KUMAR <raviranjanmth2022@gmail.com>
2026-09-08 10:30:12 +0530

    ✅ SCALENE - Rating 430 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-08

--- codechef/easy/SCALENE---Rating-430/solution.py
+++ codechef/easy/SCALENE---Rating-430/solution.py
@@ -10,3 +10,15 @@ while t > 0:
         print("NO")
 
     t -= 1
+    
+    t=int(input())
+    
+    while t > 0:
+        a , b , c = map(int, input().split())
+        
+        if a != b and b != c and a != c:
+            print("YES")
+        else:
+            print("NO")
+            
+        t -=1    
