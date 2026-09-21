8253d5f59597594823cbcc8c4742e3a27628adc0
RAVI RANJAN KUMAR <raviranjanmth2022@gmail.com>
2026-09-21 23:26:50 +0530

    ✅ OPMIN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/OPMIN/solution.py
+++ codechef/medium/OPMIN/solution.py
@@ -1,20 +1,11 @@
-t = int(input())
-
-while t > 0:
-    n = int(input())
-    a = list(map(int, input().split()))
-    
-    largest = -1
-    second = -1
-    
-    for x in a:
-        if x > largest:
-            second = largest
-            largest = x 
-        elif x > second and x != largest:
-            second = x 
-            
-    print(largest + second)
-    
-    t -= 1
-    # Your code goes here
+class Solution:
+    def count_non_minimum(self, nums):
+        minimum = min(nums)
+        cout = 0 
+        
+        for x in nums:
+            if x > minimum:
+                cout += 1
+        return cout
+        # write your code here
+        
