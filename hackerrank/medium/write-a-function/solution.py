81dea135603a318a15ec382edea122890c3e326c
RAVI RANJAN KUMAR <raviranjanmth2022@gmail.com>
2026-09-23 11:19:36 +0530

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,5 +1,15 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        print(i * i)
+def is_leap(year):
+    leap = False
     
+    # Write your logic here
+    if year % 400 == 0:
+        return True
+    elif year % 100 ==0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+    
+    return leap
+
